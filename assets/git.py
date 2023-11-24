5e94726501df3f13e019feab84f4e46324a0962b
ali foladi <foladi169@gmail.com>
2023-11-24 05:21:13 +0330

    second commit init

---
+++ assets/git.py
@@ -0,0 +1,15 @@
+# start-git=
+#     git config --global user.name "mosafe"
+#     git config --global user.email "email@test.com"
+# git init
+
+# git add .
+# git status
+# git commit -m "massage for me"
+# git remote add origin https://github.com/m0safer/ali-repository.git
+# git push -u origin master
+
+ali_git_username="mosafer"
+print(ali_git_username)
+print(type(ali_git_username))
+
