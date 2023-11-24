8c140f364c705b06c7782f4323a225b61272fa8c
mosafer <foladi169@gmail.com>
2023-11-24 04:09:14 +0330

    4th commit

---
+++ assets/git.py
@@ -0,0 +1,11 @@
+# start-git=
+#     git config --global user.name "mosafe"
+#     git config --global user.email "email@test.com"
+# git init
+
+# git add .
+# git status
+# git commit -m "massage for me"
+
+ali_git_username="mosafer"
+
