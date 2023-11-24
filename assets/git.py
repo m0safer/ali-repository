1f06032c9fa38fcb2b3b9eb5f102614136413c22
mosafer <foladi169@gmail.com>
2023-11-24 04:46:13 +0330

    first commit of new project

--- assets/git.py
+++ assets/git.py
@@ -6,6 +6,10 @@
 # git add .
 # git status
 # git commit -m "massage for me"
+# git remote add origin https://github.com/m0safer/ali-repository.git
+# git push -u origin master
 
 ali_git_username="mosafer"
+print(ali_git_username)
+print(type(ali_git_username))
 
